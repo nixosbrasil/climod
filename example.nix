2e382ef7151a0461945d4acceedd7e55848bb8c1
Hugo Sena Ribeiro <hugosenari@gmail.com>
2022-11-19 00:09:12 -0500

    Update example.nix
    make example flat

--- example.nix
+++ example.nix
@@ -1,46 +1,35 @@
 { pkgs ? import <nixpkgs> {} }:
-let
-  climod = pkgs.callPackage ./default.nix { inherit pkgs; };
-in climod {
+let climod = pkgs.callPackage ./default.nix { inherit pkgs; };
+in  climod {
   name = "demo";
   description = "Demo CLI generated";
-  target.bash.prelude = ''
-    echo "Starting..."
-  '';
   action.bash = ''
     echo Hello, world
     echo $#
     while [ $# -gt 0 ]; do
-      echo "$1"
+      echo  "$1"
       shift
     done
   '';
+  target.bash.prelude = ''
+    echo "Starting..."
+  '';
   allowExtraArguments = true;
-  subcommands = {
-    args = {
-      description = "Print args";
-      allowExtraArguments = true;
-      action.bash = ''
-        for line in "$@"; do
-          echo $line
-        done
-      '';
-    };
-    eoq = {
-      description = "Eoq subcommand";
-      subcommands = {
-        greet = {
-          description = "Greets the user";
-          flags = [
-            {
-              description = "User name";
-              keywords = ["-n" "--name" ];
-              variable = "GREET_USER";
-            }
-          ];
-          action.bash = "echo Hello, $GREET_USER!";
-        };
-      };
-    };
-  };
+  subcommands.args.description = "Print args";
+  subcommands.args.allowExtraArguments = true;
+  subcommands.args.action.bash = ''
+    for line in "$@"; do
+      echo $line
+    done
+  '';
+  subcommands.eoq.description = "Eoq subcommand";
+  subcommands.eoq.subcommands.greet.description = "Greets the user";
+  subcommands.eoq.subcommands.greet.action.bash = "echo Hello, $GREET_USER!";
+  subcommands.eoq.subcommands.greet.flags = [
+    {
+      description = "User name";
+      keywords    = ["-n" "--name" ];
+      variable    = "GREET_USER";
+    }
+  ];  
 }
